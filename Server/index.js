545820eed0537771cd1842139231305ed6e5a020
Ratnakar <ratnakargiri973@gmail.com>
2024-12-29 20:46:18 +0530

    frontend link applied

--- Server/index.js
+++ Server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT;
 
 const app = express();
 const corsOption = {
-    origin:"http://localhost:5173",
+    origin:"https://e-commerce-1-34e1.onrender.com",
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
