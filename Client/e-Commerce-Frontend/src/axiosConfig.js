c4d7e77a5ea419a5f82dbc5114007c02353db0b9
Ratnakar <ratnakargiri973@gmail.com>
2024-12-29 18:45:37 +0530

    dist added

--- Client/e-Commerce-Frontend/src/axiosConfig.js
+++ Client/e-Commerce-Frontend/src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: "http://localhost:8917/api/v1",
+    baseURL: "https://e-commerce-8xmf.onrender.com/api/v1",
     withCredentials: true
 });
 
