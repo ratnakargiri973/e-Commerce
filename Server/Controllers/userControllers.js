c2adb7f1ddb467b1735c05d3f4b007e0420ba2ee
Ratnakar <ratnakargiri973@gmail.com>
2024-12-31 09:10:34 +0530

    updated

--- Server/Controllers/userControllers.js
+++ Server/Controllers/userControllers.js
@@ -121,7 +121,7 @@ export const login = async (req, res) => {
        res.cookie("token", token, {
         httpOnly: true,
         secure: true,
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 24 * 60 * 60 * 1000,
        });
 
